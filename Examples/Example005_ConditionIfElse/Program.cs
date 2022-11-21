747aa9307f633b42c2320f6efd7ce98ff7a20d8c
sharlottakorde@gmail.com <sharlottakorde@gmail.com>
2022-11-21 16:03:05 +0300

    adding cond if/else

---
+++ Examples/Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter your name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("Wow! It is MASHA!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
