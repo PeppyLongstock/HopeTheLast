3d07458abeead595199725721a382b933b7b2af4
sharlottakorde@gmail.com <sharlottakorde@gmail.com>
2022-12-15 23:22:56 +0300

    new Random

--- Examples/Example002_HelloUser/Program.cs
+++ Examples/Example002_HelloUser/Program.cs
@@ -2,3 +2,10 @@
 string username = Console.ReadLine();
 Console.Write("Hello, ");
 Console.Write(username);
+
+int numberA = new Random().Next(1, 10);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
+int result = numberA + numberB;
+Console.WriteLine(result);
