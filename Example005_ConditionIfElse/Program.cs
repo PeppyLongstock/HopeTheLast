a1069d8501702ae46aeb20305c7185a6c6246e57
sharlottakorde@gmail.com <sharlottakorde@gmail.com>
2022-12-16 10:39:32 +0300

    Masha

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter a username: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "masha")
+{
+    Console.WriteLine("Happy to see you, Masha!");
+}
+else
+{
+    Console.Write("Hi ");
+    Console.WriteLine(username);
+}
